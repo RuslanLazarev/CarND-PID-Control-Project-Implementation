31e6d4293e70c8a63639ed846299a18a082cc465
RuslanLazarev <ruslan.lazarev@gmail.com>
2018-09-09 00:06:48 +0200

    update

--- src/PID.cpp
+++ src/PID.cpp
@@ -46,7 +46,7 @@ double PID::TotalError() {
 
 vector<double> PID::Twiddle(double cte) {
     best_err = total_err;
-    sum_dp = accumulate(dp.begin(), dp.end(),0)
+    double sum_dp = accumulate(dp.cbegin(), dp.cend(),0)
     while sum_dp > 0.2 {
         for (int i = 0, i < p.size(), i++) {
             p[i] += dp[i];
